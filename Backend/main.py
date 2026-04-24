40afd9fbe16a9102490249dc6a41802bd0d11c31
Soka_jplr <juanlazarorp@gmail.com>
2026-04-23 23:26:32 -0600

    refactor main and usuario.py with sqlModel

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated
+from Model.usuario import UsuarioPublic, Usuario
+from data_base import session_dep
+from sqlmodel import select
+
 app = FastAPI()
 
-@app.get("/idtipo/{idtipo:path}")
-async def get_precio(idtipo: str):
-    return {"precio": idtipo}
+@app.get("/")
+async def get_precio():
+    return {"saludo": "Hola mundo!"}
+
+@app.get("/usuarios", response_model=list[UsuarioPublic])
+def read_usuarios(
+    session: session_dep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+):
+    usuarios = session.exec(select(Usuario).offset(offset).limit(limit)).all()
+    return usuarios
+
+
+    
