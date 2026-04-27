018a49c1b430917266ead50632de38172aa41b9c
Soka_jplr <juanlazarorp@gmail.com>
2026-04-27 00:33:02 -0600

    add WS of categoria

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-
-from ws import usuario_ws, producto_ws
+from ws import usuario_ws, producto_ws, categoria_ws
 
 app = FastAPI()
 
 app.include_router(usuario_ws.router)
 app.include_router(producto_ws.router)
+app.include_router(categoria_ws.router)
 
 
     
